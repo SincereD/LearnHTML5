9fce5347f92298d47b07ec1bff0c8217fa3c2e8a
杜凯强 <kqdu@yellfun.com>
2017-02-08 09:47:23 +0800

    js 测试

---
+++ CharSort/config.js
@@ -0,0 +1,25 @@
+var button = document.getElementById("buttonItem");
+
+button.onclick = function a() {
+
+	console.log("+============\n");
+
+	testfunc();
+};
+
+function testfunc() {
+
+	console.log("1sdasdass");
+
+	console.log(ChineseSort(["哈", "打", "溜", "破", "呼", "走", "啊"]));
+}
+
+
+
+/*
+
+1.  1.5.3版本遗留问题
+2.  版本优化问题
+3.  新版本SDK集成测试
+
+*/
